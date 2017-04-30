9ff22c60171c56d8962c7cab33a7c5f9b5237238
Dean Bassett <deanvelopermn@gmail.com>
2017-04-30 04:41:56 -0500

    make this synchronous for now (will change later)

--- events.go
+++ events.go
@@ -54,18 +54,15 @@ func handleCommand(s *discordgo.Session, ev *discordgo.MessageCreate) {
 
         } else { // if multiple people were mentioned
             for _, user := range mentions {
-                // get each one asynchronously
-                go func() {
-                    karma, err := getKarma(user)
+                karma, err := getKarma(user)
 
-                    if err != nil {
-                        fmt.Println("Error getting karma for", user.Username, ":", err)
-                        s.ChannelMessageSend(ev.ChannelID, "Error getting karma for "+user.Username+": `"+err.Error()+"`")
-                        return
-                    }
+                if err != nil {
+                    fmt.Println("Error getting karma for", user.Username, ":", err)
+                    s.ChannelMessageSend(ev.ChannelID, "Error getting karma for "+user.Username+": `"+err.Error()+"`")
+                    return
+                }
 
-                    s.ChannelMessageSend(ev.ChannelID, fmt.Sprintf("**%s** has **%d** karma", user.Username, karma))
-                }()
+                s.ChannelMessageSend(ev.ChannelID, fmt.Sprintf("**%s** has **%d** karma", user.Username, karma))
             }
         }
     }
