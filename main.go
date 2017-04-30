f7468ba1e3b3830d6cde0ab34295b4251e2636d7
Dean Bassett <deanvelopermn@gmail.com>
2017-04-30 03:59:59 -0500

    better message

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
     Start()
 
-    fmt.Println("Press CTRL-C to exit.")
+    fmt.Println("Successfully started! When done, press CTRL-C to exit.")
     <-make(chan struct{})
     return
 }
