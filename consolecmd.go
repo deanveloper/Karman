332c4578f5d97edd598b47e38e1702ff7d9bba48
Dean Bassett <deanvelopermn@gmail.com>
2017-05-11 07:36:59 -0500

    added announce console command

--- consolecmd.go
+++ consolecmd.go
@@ -1,6 +1,8 @@
 package karman
 
 import (
+    "fmt"
+    "github.com/bwmarrin/discordgo"
     "strconv"
     "strings"
 )
@@ -25,6 +27,24 @@ func (b *Karman) Command(args []string) {
                     return
                 }
                 b.log.Println("Karma:", karma)
+            }
+        case "announce":
+            msg := fmt.Sprintf("%s", args[1:])
+            msg = "**Developer announcement:** " + msg[1:len(msg)-1] // add prefix, trim brackets
+            for _, g := range b.dg.State.Guilds {
+                go func(g *discordgo.Guild) {
+                    for _, ch := range g.Channels {
+                        _, err := b.dg.ChannelMessageSend(ch.ID, msg)
+                        if err == nil {
+                            break
+                        }
+                    }
+                }(g)
+            }
+        case "list":
+            b.log.Println("Guilds: ")
+            for _, g := range b.dg.State.Guilds {
+                b.log.Println(g.Name)
             }
         default:
             b.log.Println("Command unknown:", args[0])
